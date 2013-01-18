e8cde4449ec0f4f303d076420e735d17362df2c6
r.j.versluis@rocrail.net <>
2013-01-18 07:38:41 +0100

    guest loco: additional options

--- src/net/rocrail/androc/activities/ActGuestLoco.java
+++ src/net/rocrail/androc/activities/ActGuestLoco.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class ActGuestLoco extends ActBase {
 
@@ -31,10 +32,32 @@ public class ActGuestLoco extends ActBase {
     AddMember.setOnClickListener(new View.OnClickListener() {
         public void onClick(View v) {
           EditText et = (EditText)findViewById(R.id.guestAddress);
+          EditText id = (EditText)findViewById(R.id.guestShortID);
+          RadioButton step14 = (RadioButton)findViewById(R.id.Speedsteps14);
+          RadioButton step28 = (RadioButton)findViewById(R.id.Speedsteps28);
+          RadioButton step128 = (RadioButton)findViewById(R.id.Speedsteps128);
+          
+          RadioButton dcc = (RadioButton)findViewById(R.id.ProtocolDCC);
+          RadioButton mm  = (RadioButton)findViewById(R.id.ProtocolMM);
+
           int addr = Integer.parseInt(et.getText().toString());
+          
+          int steps = 128;
+          if( step28.isChecked() )
+            steps = 28;
+          else if( step14.isChecked() )
+            steps = 14;
+          
+          String prot = "P";
+          if( mm.isChecked() )
+            prot = "M";
+          if( dcc.isChecked() )
+            prot = ((addr > 127) ? "L":"N");
+          
           if( addr > 0 ) {
             m_RocrailService.sendMessage("lc", 
-                String.format("<lc id=\"%d\" V=\"0\"/>", addr));
+                String.format("<lc id=\"%d\" shortid=\"%s\" spcnt=\"%d\" prot=\"%s\" V=\"0\"/>", 
+                    addr, id.getText().toString(), steps, prot));
           }
           finish();
         }
